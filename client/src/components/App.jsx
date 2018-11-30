6e5913e220c260e23c688211bb056ad6799d2921
Charlie Phan <charliegphan@gmail.com>
2018-11-30 11:37:19 -0800

    add starter code for App.jsx, eslint airbnb, index.html

---
+++ client/src/components/App.jsx
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        Hello from React App
+      </div>
+    )
+  }
+}
+
+export default App;
